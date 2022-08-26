55df75356f75398230bd2a231fa7605aa07fe7f8
ANDRE SOUSA <87583186+AndreLNDEsousa@users.noreply.github.com>
2022-08-26 14:48:04 -0300

    organização JS

--- assets/firebase/app.js
+++ assets/firebase/app.js
@@ -17,4 +17,5 @@
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
 
+
   export default app
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 
-import { subscribeToHellfireClub } from "../firebase/hellfireclub.js"
+import { subscribeToHellfireClub } from "../firebase/hellfire-club.js"
 
 
 const txtName = document.getElementById('txtName')
@@ -10,15 +10,22 @@ const btnSubscribe = document.getElementById('btnSubscribe')
 
 btnSubscribe.addEventListener('click', async () => {
     const subscription = {
-        name: txtName.value ,
-        email: txtEmail.value ,
-        level: txtLevel.value , 
+        name: txtName.value,
+        email: txtEmail.value,
+        level: txtLevel.value,
         character: txtCharacter.value
     }
 
     // Salvar no banco de dados
-    const subscriptionId = await subscribeToHellfireClub (subscription)
-    console.log ( 'Inscrito com sucesso!!! ${subscriptionId}')
-})
+    const subscriptionId = await subscribeToHellfireClub(subscription)
 
-          
+    // Para limpar o formulário depois do envio
+    txtName.value = ""
+    txtEmail.value = ""
+    txtLevel.value = ""
+    txtCharacter.value = ""
+    //Mensagem de que foi enviado com sucesso
+    alert(`Inscrição ${subscriptionId} adicionada com sucesso!`)
+    }
+)
+
